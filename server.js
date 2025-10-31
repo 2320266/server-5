5f58d1b18eda1c16efc06f7c055140612c5eab0d
232-266 <206805069+2320266@users.noreply.github.com>
2025-10-31 19:27:39 +0900

    06

--- server.js
+++ server.js
@@ -5,8 +5,34 @@ const app = new Hono();
 app.use('/*', serveStatic({ root: './public' }));
 
 // GETリクエストに対する処理
-app.get('/api', async (c) => {
-  return c.json({ message: 'GET' });
+app.get('/api/:name/:rank', (c) => {
+  const name = c.req.param('name');
+  const rank = c.req.param('rank');
+
+  return c.json({message: 'GET', param: { name, rank } });
+});
+
+// POSTリクエストに対する処理
+app.post('/api', async (c) => {
+  const payload = await c.req.parseBody();
+  const name = payload['name'];
+  const rank = payload['rank'];
+  return c.json({ message: 'POST', name, rank });
+});
+
+// PUTリクエストに対する処理
+app.put('/api/:name', async (c) => {
+  const name = c.req.param('name');
+  const body = await c.req.json();
+  //const name = body['name'];
+  const rank = body.rank;
+  return c.json({ message: 'PUT',json: { name, rank } });
+});
+
+// DELETEリクエストに対する処理
+app.delete('/api/:name', (c) => {
+  const name = c.req.param('name');
+  return c.json({ message: 'DELETE' });
 });
 
 Deno.serve(app.fetch);
